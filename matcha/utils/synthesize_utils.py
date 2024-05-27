7aba40059de8b9702b639cdb6dbac04df6437e83
RO HOON <84008896+renslightsaber@users.noreply.github.com>
2024-05-28 07:58:33 +0900

    Update synthesize_utils.py

--- matcha/utils/synthesize_utils.py
+++ matcha/utils/synthesize_utils.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+# import argparse
 import datetime as dt
 from pathlib import Path
 # %matplotlib inline
@@ -15,24 +16,27 @@ import numpy as np
 import torch
 import torch.nn as nn
 
+# Matcha imports
+from matcha.models.matcha_tts import MatchaTTS
+from matcha.text import *
+from matcha.utils.model import denormalize
+from matcha.utils.utils import get_user_data_dir, intersperse
+
 # os.environ["CUDA_VISIBLE_DEVICES"]="0"
 # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # HiFi-GAN
-def get_hifigan( device = torch.device("cuda" if torch.cuda.is_available() else "cpu") ):
+def get_hifigan(device):
     vocoder, vocoder_train_setup, denoiser = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_hifigan', trust_repo=True) ## Worked
     print(f"DownLoaded | NVIDIA's HiFi-GAN from torch hub | SR: 22050")
     vocoder.eval()
     vocoder = vocoder.to(device)
     denoiser = denoiser.to(device)
 
-    return vocoder, vocoder_train_setup, denoiser 
+    return vocoder, vocoder_train_setup, denoiser
 
-# vocoder, vocoder_train_setup, denoiser = get_hifigan(device)
-
-# Synthesize Helper functions
 @torch.inference_mode()
-def process_text(text: str):
+def process_text(text, device =  torch.device("cuda" if torch.cuda.is_available() else "cpu")):
     x = torch.tensor(intersperse(text_to_sequence(text, ['english_cleaners2']), 0),dtype=torch.long, device=device)[None]
     x_lengths = torch.tensor([x.shape[-1]],dtype=torch.long, device=device)
     x_phones = sequence_to_text(x.squeeze(0).tolist())
@@ -44,7 +48,13 @@ def process_text(text: str):
     }
 
 @torch.inference_mode()
-def synthesise(text, spks=None, n_timesteps = 10, length_scale=1.0, temperature = 0.667, device = device):
+def synthesise(text, 
+               model,
+               spks=None, 
+               n_timesteps = 10, 
+               length_scale=1.0, 
+               temperature = 0.667,
+               device =  torch.device("cuda" if torch.cuda.is_available() else "cpu")):
     text_processed = process_text(text)
     start_t = dt.datetime.now()
     output = model.synthesise(
@@ -59,12 +69,14 @@ def synthesise(text, spks=None, n_timesteps = 10, length_scale=1.0, temperature 
     output.update({'start_t': start_t, **text_processed})
     return output
 
+
 @torch.inference_mode()
-def to_waveform(mel, vocoder, denoising_strength = 0.00025):
+def to_waveform(mel, vocoder, denoiser, denoising_strength = 0.00025):
     audio = vocoder(mel).clamp(-1, 1)
-    audio = denoiser(audio.squeeze(0), strength=0.00025).cpu().squeeze()
+    audio = denoiser(audio.squeeze(0), strength=denoising_strength).cpu().squeeze()
     return audio.cpu().squeeze()
-    
+
+
 def save_to_folder(filename: str, output: dict, folder: str):
     folder = Path(folder)
     folder.mkdir(exist_ok=True, parents=True)
@@ -72,35 +84,35 @@ def save_to_folder(filename: str, output: dict, folder: str):
     sf.write(folder / f'{filename}.wav', output['waveform'], 22050, 'PCM_24')
 
 
-# Synthesize-to-speech function
-def synthesis_to_speech(texts = ["I really admire my professor most in the world"], 
+def synthesis_to_speech(texts, # = ["I really admire my professor most in the world"], 
+                        model,
+                        vocoder, # = None,
+                        denoiser, 
+                        denoising_strength = 0.00025,
                         spks=None, 
-                        n_timesteps = 10,    # Number of ODE Solver steps
-                        length_scale=1.0,    # Changes to the speaking rate
-                        temperature = 0.667, # Sampling temperature
+                        n_timesteps = 10,
+                        length_scale = 1.0, 
+                        temperature = 0.7,
                         save_wav=False, 
                         OUTPUT_FOLDER = './',
-                        device = device):
+                        device =  torch.device("cuda" if torch.cuda.is_available() else "cpu")):
     outputs, rtfs = [], []
     rtfs_w = []
     for i, text in enumerate(tqdm(texts)):
-        output = synthesise(text, spks=None, n_timesteps = 10, length_scale=1.0, temperature = 0.667) #, torch.tensor([15], device=device, dtype=torch.long).unsqueeze(0))
-        output['waveform'] = to_waveform(output['mel'], vocoder)
+        output = synthesise(text, model = model, spks=spks, n_timesteps = n_timesteps, length_scale=length_scale, temperature = temperature) #, torch.tensor([15], device=device, dtype=torch.long).unsqueeze(0))
+        output['waveform'] = to_waveform(output['mel'], vocoder, denoiser,  denoising_strength =  denoising_strength)
 
         # Compute Real Time Factor (RTF) with HiFi-GAN
         t = (dt.datetime.now() - output['start_t']).total_seconds()
         rtf_w = t * 22050 / (output['waveform'].shape[-1])
 
         ## Pretty print
-        print(f"{'*' * 53}")
-        print(f"Input text - {i}")
-        print(f"{'-' * 53}")
+        print(f"Input text: ")
         print(output['x_orig'])
-        print(f"{'*' * 53}")
-        print(f"Phonetised text - {i}")
         print(f"{'-' * 53}")
+        print(f"Phonetised text: ")
         print(output['x_phones'])
-        print(f"{'*' * 53}")
+        print(f"{'-' * 53}")
         print(f"RTF:\t\t{output['rtf']:.6f}")
         print(f"RTF Waveform:\t{rtf_w:.6f}")
         rtfs.append(output['rtf'])
@@ -116,3 +128,4 @@ def synthesis_to_speech(texts = ["I really admire my professor most in the world
         print("Not saving")
     
     return output, rtfs, rtfs_w
+
